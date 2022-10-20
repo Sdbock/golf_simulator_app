b77ac3fe9c9e1e08949f53589eee45238d334c59
Sean Bock <seanbock@g.harvard.edu>
2022-10-19 23:10:44 -0400

    adding back in shiny.info

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 
 library(shinythemes)
-#library(shiny.info)
+library(shiny.info)
 library(shiny)
 library(tidyr)
 library(dplyr)
@@ -44,7 +44,7 @@ ui <-
 navbarPage(
   shinybrowser::detect(),
   title = "Golf Match Simulator",
-  #header = display(p(style = "margin: 0;", "Created by ", a(href = "https://www.seanbock.com",                                                                                         target = "_blank", "Sean Bock")), type = "powered_by"),
+  header = display(p(style = "margin: 0;", "Created by ", a(href = "https://www.seanbock.com",                                                                                         target = "_blank", "Sean Bock")), type = "powered_by"),
   theme = my_theme,
   tabPanel("Simulator",fluidPage(theme = my_theme,
                                  tags$style('.alert {background-color: transparent}; 
